b3153a6a630224b3743d80884590bc4b9fad2988
fershik <dfernanditac@hotmail.com>
2023-03-12 04:15:02 -0500

    add pandas

--- app/main.py
+++ app/main.py
@@ -1,23 +1,32 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
 
-  data = read_csv.read_csv('data.csv')
-  
-
   ## Solucion graficar el % de poblacion en pie
   # filtrar que solo muestre latinoamerica
+
+  '''
 
   data= list(filter(lambda item: item['Continent']== 'South America',data))
   countries = list(map(lambda x:x['Country/Territory'],data))
   percentajes = list(map(lambda x:x['World Population Percentage'],data))
   charts.generate_pie_chart(countries,percentajes)
-  
+  '''
+  #Codigo en pandas
+  df = pd.read_csv("data.csv")  # Nos ahorramos el método creado read_csv.py
+  df = df[df['Continent'] == 'Africa']
+  countries = df['Country/Territory'].values
+  percentajes = df['World Population Percentage'].values
+
+  charts.generate_pie_chart(countries,percentajes)
 
   ## Solucion Poblacion por pais y año
   
+
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   
   result = utils.population_by_country(data,country)
